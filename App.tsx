72cd43a731c342877619247d46fdef795402bc14
manucastelnovo <marnutskate@gmail.com>
2026-05-21 11:55:21 -0300

    update WebView source and add local storage initialization script

--- App.tsx
+++ App.tsx
@@ -2,13 +2,27 @@ import { StatusBar, StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
+const UBICACION = '261.7.1';
+
+const PRESET_LOCALSTORAGE = `
+  try {
+    localStorage.setItem('ub_eleccion',     'ASOCIACION NACIONAL REPUBLICANA');
+    localStorage.setItem('ub_departamento', '7-ITAPUA');
+    localStorage.setItem('ub_distrito',     '1-ALTO VERA');
+    localStorage.setItem('ub_localidad',    '308-ALTO VERA');
+    localStorage.setItem('ubicacion',       '${UBICACION}');
+  } catch (e) {}
+  true;
+`;
+
 function App() {
   return (
     <SafeAreaProvider>
       <StatusBar hidden />
       <View style={styles.container}>
         <WebView
-          source={{ uri: 'file:///android_asset/simulador/index.html' }}
+          source={{ uri: `file:///android_asset/simulador/sufragio.html?ubicacion=${UBICACION}` }}
+          injectedJavaScriptBeforeContentLoaded={PRESET_LOCALSTORAGE}
           originWhitelist={['*']}
           javaScriptEnabled
           domStorageEnabled
